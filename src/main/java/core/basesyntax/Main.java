ce9c8be8d4868e9c3691c9605f1458cb65d01150
Martyna Szałańska <szalanskatita2002@gmail.com>
2026-03-28 22:24:10 +0100

    fix checkstyle - fix import and add empty line before class

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,13 @@ package core.basesyntax;
 
 import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.model.Storage;
+import core.basesyntax.service.DataConverter;
+import core.basesyntax.service.FileReader;
+import core.basesyntax.service.FileWriter;
+import core.basesyntax.service.OperationHandler;
+import core.basesyntax.service.OperationStrategy;
+import core.basesyntax.service.ReportGenerator;
+import core.basesyntax.service.ShopService;
 import core.basesyntax.service.impl.DataConverterImpl;
 import core.basesyntax.service.impl.FileReaderImpl;
 import core.basesyntax.service.impl.FileWriterImpl;
@@ -12,18 +19,13 @@ import core.basesyntax.service.strategy.BalanceOperation;
 import core.basesyntax.service.strategy.PurchaseOperation;
 import core.basesyntax.service.strategy.ReturnOperation;
 import core.basesyntax.service.strategy.SupplyOperation;
-import core.basesyntax.service.DataConverter;
-import core.basesyntax.service.FileReader;
-import core.basesyntax.service.FileWriter;
-import core.basesyntax.service.OperationHandler;
-import core.basesyntax.service.OperationStrategy;
-import core.basesyntax.service.ReportGenerator;
-import core.basesyntax.service.ShopService;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 public class Main {
+
     public static void main(String[] arg) throws IOException {
 
         Storage storage = new Storage();
